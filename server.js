bd80d014788c9882654bec4e497d16226582a9f1
heyho-time <heyho9292@gmail.com>
2022-11-26 14:26:14 +0900

    dynamic resolver,  root를 분해

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 import { ApolloServer, gql } from "apollo-server";
 
-// package.json에서 "type":"module" 을 해줬기 때문에 import 문법을 쓸 수 있다.
-// 아니면
-// const {ApolloServer, gql} require("apollo-server") 이런 문법을 써야함.
-
 const tweets = [
   {
     id: "1",
@@ -57,24 +53,14 @@ const typeDefs = gql`
 `;
 
 const resolvers = {
-  // 누군가 field를 요청했을 때 실제로 호출될 함수.
   Query: {
     allTweets() {
       return tweets;
     },
-    // tweet() {
-    //   console.log("I'm called");
-    //   return null;
-    // },
-    // ping() {
-    //   return "pong";
-    // },
+
     tweet(root, { id }) {
-      // console.log(args, "args");
       return tweets.find((tweet) => tweet.id === id);
     },
-    //args는 유저가 보낸 argument
-    //apollo server가 resolver를 부를때 root 인자를 줌.
 
     allUsers() {
       console.log("allUsers called");
@@ -91,27 +77,11 @@ const resolvers = {
     },
   },
   User: {
-    fullName(root) {
-      console.log("full name called");
-      console.log(root);
-      return "hello";
+    fullName({ firstName, lastName }) {
+      return `${firstName} ${lastName}`;
     },
   },
 };
-
-// allUsers가 먼저 호출돼 allUsers called 출력,. users 데이터에 fullName field가 없는걸 알아채고 resolver의 fullName을 사용한다.
-
-// 콘솔에 이렇게 찍힌다.
-// allUsers called
-// full name called
-// { id: '1', firstName: 'harry', lastName: 's' }
-// full name called
-// { id: '2', firstName: 'heyho', lastName: 'yo' }
-// allUsers called
-// full name called
-// { id: '1', firstName: 'harry', lastName: 's' }
-// full name called
-// { id: '2', firstName: 'heyho', lastName: 'yo' }
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
